56d64a3a2face434976c669952607b4e4c808e08
Jose Juarez <Jjuareze2002@gmail.com>
2024-04-28 22:09:09 -0500

    fixing some bugs

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -6,4 +6,4 @@ export {
     fetchData, 
     postData, 
     updateData, 
-}
+}; 
--- client/src/utils/updateData.js
+++ client/src/utils/updateData.js
@@ -4,7 +4,14 @@ const updateData = async (
     endpoint,
     data, 
 ) => {
-   
+   try{
+     fetch(endpoint, {
+        method:'PUT', 
+        headers: {'Content-Type' : 'application/json'}
+     })
+   }catch(error){
+    console.error('could not update:', error)
+   }
 }
 
 export {
