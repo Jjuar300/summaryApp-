a84af76a8e0e6e1ee3c1c184f42512f079f24e74
Jose Juarez <Jjuareze2002@gmail.com>
2024-07-02 22:31:43 -0500

    modifiying the text editor

--- client/src/pages/Summary/index.jsx
+++ client/src/pages/Summary/index.jsx
@@ -4,15 +4,49 @@ import Actions from '../home/Actions'
 import {LexicalComposer} from '@lexical/react/LexicalComposer';
 import {PlainTextPlugin} from '@lexical/react/LexicalPlainTextPlugin';
 import {ContentEditable} from '@lexical/react/LexicalContentEditable';
-import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
-import {OnChangePlugin} from '@lexical/react/LexicalOnChangePlugin';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary'; 
+
 import { useMemo } from "react";
 
-const initialConfig = {
- namespace: 'MyEditor', 
-}; 
 
 export default function index() {
+
+
+const customContent = useMemo(() => {
+  return (
+    <ContentEditable
+     style={{
+      position: 'relative',
+      borderColor: 'rgba(255,211,2,0.68)',
+      border: '2px solid red',
+      borderRadius: '5px', 
+      padding: '10px',
+      width:'12rem', 
+     }}
+    />
+  )
+}, []); 
+
+const customPlaceholder = useMemo(() => {
+return (
+  <div
+  style={{
+    position: 'absolute', 
+    top:'30', 
+    left:'30', 
+  }}
+  >
+    Enter some text ...
+  </div>
+)
+}, [])
+
+const lexicalConfig = {
+  namespace: 'Rich text editor', 
+  onError: (error) => {
+    console.log("ERROR", error)
+  }
+};
   return (
     <div>
    
@@ -26,12 +60,12 @@ export default function index() {
    }}
    >
 
-    <LexicalComposer initialConfig={initialConfig}>
+    <LexicalComposer initialConfig={lexicalConfig}>
        <PlainTextPlugin
-       contentEditable={ContentEditable}
-       placeholder={<h1>text editor</h1>}
+       contentEditable={customContent}
+       placeholder={customPlaceholder}
+       ErrorBoundary={LexicalErrorBoundary}
        />
-       <HistoryPlugin/>
     </LexicalComposer>
    </Box>
 
