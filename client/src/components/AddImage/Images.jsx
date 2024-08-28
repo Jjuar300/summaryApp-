3cc907b9416942e9de1b7e2008200287ca7c6989
Jose Juarez <Jjuareze2002@gmail.com>
2024-08-27 22:23:48 -0500

    working on uploading images

--- client/src/components/AddImage/Images.jsx
+++ client/src/components/AddImage/Images.jsx
@@ -1,39 +1,33 @@
 import "./styles/images.css";
 import { useDispatch, useSelector } from "react-redux";
 import { setFileName, setImageClick } from "../../Redux/imageContainer";
-import {IKImage} from 'imagekitio-react'; 
+import { IKImage, IKUpload } from "imagekitio-react";
 import LazyLoad from "react-lazyload";
+import { useMemo, useState } from "react";
 
 export default function Images() {
   const urlEnpoint = import.meta.env.VITE_IMAGEKIT_URL_KEY;
-  const publickey = import.meta.env.VITE_PUBLIC_KEY; 
+  const publickey = import.meta.env.VITE_PUBLIC_KEY;
   const isTranslate = useSelector((state) => state.imageContainer.isImageClick);
   const dispatch = useDispatch();
 
-const images = [
-    'leaf.jpg', 
-    'rocks.jpg', 
-    'tower.jpg', 
-    'window.jpg', 
-    'sunrise.jpg', 
-  ]
+  const images = [
+    "leaf.jpg",
+    "rocks.jpg",
+    "tower.jpg",
+    "window.jpg",
+    "sunrise.jpg",
+  ];
 
   const handleClick = (imageName) => {
     dispatch(setFileName(imageName));
     if (imageName) return dispatch(setImageClick(!isTranslate));
   };
 
-  return (
-  <div>
-
-      <div className="imagetitle">Images</div>
- <div className={"images"}>
-        {images.map((image, index) => (
-       
-        <LazyLoad
-        key={index}
-        >
-            <IKImage
+  const mapImages = useMemo(() => {
+    return images.map((image, index) => (
+      <LazyLoad key={index}>
+        <IKImage
           onClick={() => handleClick(image)}
           className="darkimage"
           loading="lazy"
@@ -43,14 +37,18 @@ const images = [
           urlEndpoint={urlEnpoint}
           publicKey={publickey}
           path={`${image}`}
-          />
-        </LazyLoad>
-       
-       ))}
+        />
+      </LazyLoad>
+    ));
+  }, [images]);
 
+  return (
+    <div>
+      <div className="imagetitle">Images</div>
+      <div className={"images"}>
+        {mapImages}
         <button className="upload">UPLOAD</button>
       </div>
-
-  </div>
+    </div>
   );
 }
