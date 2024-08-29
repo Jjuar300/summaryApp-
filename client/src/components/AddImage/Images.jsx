46902abc115b003f44ece2d3ca6ffeb8bbd35283
Jose Juarez <Jjuareze2002@gmail.com>
2024-08-28 22:36:25 -0500

    still working ong imagekit

--- client/src/components/AddImage/Images.jsx
+++ client/src/components/AddImage/Images.jsx
@@ -1,15 +1,61 @@
 import "./styles/images.css";
 import { useDispatch, useSelector } from "react-redux";
 import { setFileName, setImageClick } from "../../Redux/imageContainer";
-import { IKImage, IKUpload } from "imagekitio-react";
+import { IKContext, IKImage, IKUpload } from "imagekitio-react";
 import LazyLoad from "react-lazyload";
 import { useMemo, useState } from "react";
+import imagekit from 'imagekit'
+import ImageKit from "imagekit";
 
 export default function Images() {
   const urlEnpoint = import.meta.env.VITE_IMAGEKIT_URL_KEY;
   const publickey = import.meta.env.VITE_PUBLIC_KEY;
   const isTranslate = useSelector((state) => state.imageContainer.isImageClick);
   const dispatch = useDispatch();
+  const [selectedFile, setSelectedFile] = useState(null); 
+
+  // const imagekit = new ImageKit ({
+  //  url: urlEnpoint, 
+  //  publicKey: publickey, 
+   
+  // }); 
+
+  const authenticator = async () => {
+    try {
+      const response = await fetch('http://localhost:3004/auth'); 
+    if(!response.ok){
+      const errorText = await response.text(); 
+      throw new Error(`Request failed with status ${response.status}: ${errorText}`); 
+    }; 
+
+    const data = await response.json(); 
+    const {signature, expire, token} = data;
+    return {signature, expire, token}; 
+
+    } catch (error) {
+      throw new Error(`Authentication request fail ${error.message}`)
+    }
+  }
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]); 
+  }; 
+
+  const handleUpload = async () => {
+    try {
+      
+      const file = selectedFile; 
+      const fileName = file.name; 
+      const uploadResult = await imagekit.upload({
+        file: file, 
+        fileName: fileName, 
+      })
+
+      console.log('upload successful:', uploadResult); 
+    } catch (error) {
+      console.error('upload error:', error); 
+    }
+  }
 
   const images = [
     "leaf.jpg",
@@ -23,6 +69,8 @@ export default function Images() {
     dispatch(setFileName(imageName));
     if (imageName) return dispatch(setImageClick(!isTranslate));
   };
+
+ console.log('selectedFile:', selectedFile?.name)
 
   const mapImages = useMemo(() => {
     return images.map((image, index) => (
@@ -47,7 +95,18 @@ export default function Images() {
       <div className="imagetitle">Images</div>
       <div className={"images"}>
         {mapImages}
-        <button className="upload">UPLOAD</button>
+        {/* <button className="upload" onClick={handleUpload} >UPLOAD</button> */}
+        {/* <input type="file" onChange={handleFileChange} /> */}
+      
+      <IKContext
+      authenticationEndpoint='https://localhost3004:/auth'
+      publicKey='public_GjEtjvvBdROHsJ46QIVXwiNKWGo'
+      urlEndpoint={urlEnpoint}
+      authenticator={authenticator}
+      >
+      <IKUpload
+      fileName={selectedFile?.name}/>
+      </IKContext>
       </div>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3004;
 const app = express();
 const routes = require("./routes");
+const Imagekit = require('imagekit'); 
 
 app.use(cors("*"));
 app.use(express.urlencoded({ extended: false }));
@@ -19,6 +20,20 @@ try {
 } catch (error) {
   console.log(`${error}: did not connect`);
 }
+
+//imagekit
+
+const imagekit = new Imagekit({
+  urlEndpoint: process.env.VITE_IMAGEKIT_URL_KEY,
+  publicKey: process.env.VITE_IMAGEKIT_PLUBLIC_KEY, 
+  privateKey: process.env.VITE_IMAGEKIT_PRIVATE_KEY, 
+});
+
+app.get('/auth', (req,res) => {
+  const result = imagekit.getAuthenticationParameters(); 
+  res.send(result); 
+})
+
 
 app.listen(PORT, () => {
   console.log("SERVER RUNNING ON PORT: ", PORT);
