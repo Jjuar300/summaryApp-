c67e85f13a0b59f9e713e7f87047991f3e7e6a85
Jose Juarez <Jjuareze2002@gmail.com>
2025-06-09 23:15:36 -0500

    updating the landing page

--- client/src/pages/LandingPage/index.jsx
+++ client/src/pages/LandingPage/index.jsx
@@ -1,10 +1,26 @@
 import { SignInButton } from "@clerk/clerk-react";
+import './styles/index.css'
+import computerGirl from './assets/GirlComputer.png';
+import darkGrayPolygon from './assets/darkGrayPolygon.png';
+import lightGrayPolygon from './assets/lightGrayPolygon.png';
+import softGrayPolygon from './assets/softGrayPolygon.png';
+import NoteTakingVideo from './assets/NoteTakingLanding1.mp4'
 
 export default function index() {
   return (
     <>
-      <h1>Welcome to the landing page</h1>
-      <SignInButton/>
+      <h1 className="title">Collect your thoughts.</h1>
+        <SignInButton>
+          <button className="signIn" >Sign up</button>
+        </SignInButton>
+        <span className="takeNotes" >Take notes the simple way, Forever.</span>
+        <img className="computerGirl" src={computerGirl} />
+        <img className="darkgraypolygon" src={darkGrayPolygon} />
+        <img className="lightgraypolygon" src={lightGrayPolygon} />
+        <img className="softgraypolygon" src={softGrayPolygon} />
+        <video autoPlay muted loop className="noteTakingVideo">
+          <source src={NoteTakingVideo} />
+        </video>
     </>
   );
 }
