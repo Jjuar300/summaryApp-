e835bfcd32a8e306bc381fbf8fa260e3970776f5
Jose Juarez <Jjuareze2002@gmail.com>
2025-06-10 20:53:58 -0500

    complete: finish building the landing page

--- client/src/pages/LandingPage/index.jsx
+++ client/src/pages/LandingPage/index.jsx
@@ -1,26 +1,38 @@
 import { SignInButton } from "@clerk/clerk-react";
-import './styles/index.css'
-import computerGirl from './assets/GirlComputer.png';
-import darkGrayPolygon from './assets/darkGrayPolygon.png';
-import lightGrayPolygon from './assets/lightGrayPolygon.png';
-import softGrayPolygon from './assets/softGrayPolygon.png';
-import NoteTakingVideo from './assets/NoteTakingLanding1.mp4'
+import "./styles/index.css";
+import computerGirl from "./assets/GirlComputer.png";
+import darkGrayPolygon from "./assets/darkGrayPolygon.png";
+import lightGrayPolygon from "./assets/lightGrayPolygon.png";
+import softGrayPolygon from "./assets/softGrayPolygon.png";
+import NoteTakingVideo from "./assets/NoteTakingLanding1.mp4";
+import Ccorporation from "./assets/Ccorporation.png";
+import NoteLogo from "./assets/NoteLogo.png";
 
 export default function index() {
   return (
     <>
       <h1 className="title">Collect your thoughts.</h1>
-        <SignInButton>
-          <button className="signIn" >Sign up</button>
-        </SignInButton>
-        <span className="takeNotes" >Take notes the simple way, Forever.</span>
-        <img className="computerGirl" src={computerGirl} />
-        <img className="darkgraypolygon" src={darkGrayPolygon} />
-        <img className="lightgraypolygon" src={lightGrayPolygon} />
-        <img className="softgraypolygon" src={softGrayPolygon} />
-        <video autoPlay muted loop className="noteTakingVideo">
-          <source src={NoteTakingVideo} />
-        </video>
+      <SignInButton>
+        <button className="signIn">Sign up</button>
+      </SignInButton>
+      <span className="takeNotes">Take notes the simple way, Forever.</span>
+      <img className="computerGirl" src={computerGirl} />
+      <img className="darkgraypolygon" src={darkGrayPolygon} />
+      <img className="lightgraypolygon" src={lightGrayPolygon} />
+      <img className="softgraypolygon" src={softGrayPolygon} />
+
+      <div className="copyRights">
+        <img className="Ccorporation" src={Ccorporation} />
+        <span>2025 Noto</span>
+      </div>
+
+      <div className="logo">
+        <img className="NoteLogo" src={NoteLogo} />
+        <span className="LogoTitle">Noto</span>
+      </div>
+      <video autoPlay muted loop className="noteTakingVideo">
+        <source src={NoteTakingVideo} />
+      </video>
     </>
   );
 }
