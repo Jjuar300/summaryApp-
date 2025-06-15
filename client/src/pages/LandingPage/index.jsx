47c0ce0e032274741ef227509abbf003cfd55e5e
Jose Juarez <Jjuareze2002@gmail.com>
2025-06-14 22:15:18 -0500

    implementing stripe into react

--- client/src/pages/LandingPage/index.jsx
+++ client/src/pages/LandingPage/index.jsx
@@ -7,14 +7,32 @@ import softGrayPolygon from "./assets/softGrayPolygon.png";
 import NoteTakingVideo from "./assets/NoteTakingLanding1.mp4";
 import Ccorporation from "./assets/Ccorporation.png";
 import NoteLogo from "./assets/NoteLogo.png";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_live_51NF8hxKBWAiPiCSPtcoQCx48lHOJtJO2DPNIlVCm3oWWHLqM6UAhKHIJINBAYP8IoRBZiqIe5Q7tKEzP0MWiOkAY003QNuFgTR"
+);
 
 export default function index() {
+  const fetchClientSecret = async () => {
+    return fetch("/create-checkout-session", { method: "POST" })
+      .then((response) => response.json())
+      .then((json) => json.checkoutSessionClientSecret);
+  };
+
   return (
     <>
-      <h1 className="title">Collect your thoughts.</h1>
+      {/* <h1 className="title">Collect your thoughts.</h1>
       <SignInButton>
-        <button className="signIn">Sign up</button>
-      </SignInButton>
+        <button className="signIn">Start free trial</button>
+      </SignInButton> */}
+
+      <Elements stripe={stripePromise} >
+        <button className="signIn">Start free trial</button>
+
+      </Elements>
+    
       <span className="takeNotes">Take notes the simple way, Forever.</span>
       <img className="computerGirl" src={computerGirl} />
       <img className="darkgraypolygon" src={darkGrayPolygon} />
