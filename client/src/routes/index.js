4d2e3dc9e531876c9d1c419f3bcebfc37dc49ab1
Jose Juarez <Jjuareze2002@gmail.com>
2025-08-30 18:54:36 -0500

    fixing deployment

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,6 +1,6 @@
 import SignedInRoutes from './SignInRoutes'
 import SignedOutRoutes from './SignedOutRoute'
-import SubscriptionPlan from './Subscription'
+import SubscriptionPlan from './Subscription/Index'
 
 export {
     SignedInRoutes, 
