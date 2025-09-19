bcee19fa5134de0cb35dba2e94363345fb861c03
Jose Juarez <Jjuareze2002@gmail.com>
2025-09-18 20:37:19 -0500

    fixing cors 4

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.get("*", (req, res) => {
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin:[ `$https://noto-cient.onrender.com/create-checkout-session`],
+    origin:[ `$https://noto-cient.onrender.com`],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
