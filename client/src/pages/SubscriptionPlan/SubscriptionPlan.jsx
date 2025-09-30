a84359fa20dba1968c90943fa768ce2e649a46c9
Jose Juarez <Jjuareze2002@gmail.com>
2025-09-29 21:37:12 -0500

    fixing browseSpace 9

--- client/src/pages/SubscriptionPlan/SubscriptionPlan.jsx
+++ client/src/pages/SubscriptionPlan/SubscriptionPlan.jsx
@@ -98,6 +98,7 @@ export default function SubscriptionPlan() {
 
   return (
     <div>
+      <button onClick={() => getSubscriptionPlan()} >get user payment</button>
       <UserAvatar
         inlineStyle={{
           position: "absolute",
